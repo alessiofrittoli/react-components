44b1bbd9566cc5268bc5fa0150dab2a263b77e93
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2025-05-15 20:17:38 +0200

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup'
+
+const enableSourcemap = process.env.NODE_ENV !== 'production'
+
+export default defineConfig( {
+	entry		: [ 'src/**/*.ts' ],
+	format		: [ 'cjs', 'esm' ],
+	dts			: true,
+	splitting	: false,
+	shims		: false,
+	skipNodeModulesBundle: true,
+	clean		: true,
+	treeshake	: true,
+	minify		: true,
+	sourcemap	: enableSourcemap,
+} )

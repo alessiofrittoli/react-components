bb6ce855e2826f54899eab01aa0e4d7332e1c904
Alessio Frittoli <35973186+alessiofrittoli@users.noreply.github.com>
2025-05-18 19:09:03 +0200

    feature/InView-HOC (#5)
    * feat: add `InView` Hight Order Component
    
    * tests: add `InView` unit tests
    
    * docs: add `InView` API Reference

---
+++ __tests__/HOC/InView.test.tsx
@@ -0,0 +1,97 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { InView } from '@/client'
+import { useInView as _useInView, type UseInViewReturnType } from '@alessiofrittoli/react-hooks'
+
+
+jest.mock( '@alessiofrittoli/react-hooks', () => ( {
+	useInView: jest.fn(),
+} ) )
+
+
+type UseInViewReturn	= Partial<UseInViewReturnType>
+type MockedUseInView	= jest.Mock<UseInViewReturn, Parameters<typeof _useInView>>
+const useInView			= _useInView as MockedUseInView
+
+
+describe( 'InView', () => {
+
+	afterEach( () => {
+		jest.clearAllMocks().resetModules()
+	} )
+
+
+	it( 'mounts/unmounts children when mountInView is true (default)', () => {
+
+		useInView.mockReturnValue( { inView: true } )
+		
+		const { rerender } = render(
+			<InView>
+				<span>Visible</span>
+			</InView>
+		)
+
+		expect( screen.getByText( 'Visible' ) )
+			.toBeInTheDocument()
+
+		useInView.mockReturnValue( { inView: false } )
+
+		rerender(
+			<InView>
+				<span>Visible</span>
+			</InView>
+		)
+
+		expect( screen.queryByText( 'Visible' ) )
+			.not.toBeInTheDocument()
+	
+	} )
+
+
+	it( 'always renders children when mountInView is false, regardless of inView', () => {
+
+		useInView.mockReturnValue( { inView: false } )
+
+		render(
+			<InView mountInView={ false }>
+				<span>Always mounted</span>
+			</InView>
+		)
+
+		expect( screen.getByText( 'Always mounted' ) )
+			.toBeInTheDocument()
+
+	} )
+	
+	
+	it( 'accepts functions as children', () => {
+
+		render(
+			<InView mountInView={ false }>
+				{ () => <div>Function children</div> }
+			</InView>
+		)
+
+		expect( screen.getByText( 'Function children' ) )
+			.toBeInTheDocument()
+
+	} )
+
+
+	it( 'passes the result of useInView to children function', () => {
+
+		const result: UseInViewReturn = { inView: true, isExiting: false }
+		
+		useInView.mockReturnValue( result )
+
+		const children = jest.fn( () => <span>Child</span> )
+		
+		render(
+			<InView>{ children }</InView>
+		)
+
+		expect( children ).toHaveBeenCalledWith( result )
+
+	} )
+
+} )
